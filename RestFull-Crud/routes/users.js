80c7c80160f03deff38ae992fe4671d22a11b74a
Wu-Chong <wu.chong.lin@iisgalvanimi.edu.it>
2020-01-24 11:58:05 +0000

    2

--- RestFull-Crud/routes/users.js
+++ RestFull-Crud/routes/users.js
@@ -9,15 +9,30 @@ const config = {
 }
 
 /* GET users listing. */
-router.get('/attori', function(req, res, next) {
+router.get('/cittadino', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);  // ... error check
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from Customer', (err, result) => {
+    sqlRequest.query('select * from Cittadino', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
+    });
+  });
+});
+
+router.get('/search/:name', function(req, res, next) {
+  sql.connect(config, err => {
+    // ... error check
+    if(err) console.log(err);
+    // Query
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(`select * from Cittadino where nome = '${req.params.name}'`, (err, result) => {
+        // ... error checks
+        if (err) console.log(err);
+
+        res.send(result);
     });
   });
 });
